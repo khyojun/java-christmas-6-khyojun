dd915fdcf2a74868eb6aacfb74ef9ff4ae5a63e8
김효준 <khyojun1104@gmail.com>
2023-11-13 18:24:23 +0900

    feat(christmas): 주문 메뉴 입력 전 출력물 입력!

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -21,4 +21,8 @@ public class OutputView {
     public void printError(String message) {
         System.out.println(message);
     }
+
+    public void beforeInputMenu() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+    }
 }
