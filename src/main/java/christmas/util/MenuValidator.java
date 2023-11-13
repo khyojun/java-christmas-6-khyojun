db1c9bb17ed714845eaf5fa35bc74121dd7521c7
김효준 <khyojun1104@gmail.com>
2023-11-13 19:23:56 +0900

    feat(christmas): 메뉴의 개수가 1 이상 20 이하의 숫자가 오지 않는 예외 처리!

--- src/main/java/christmas/util/MenuValidator.java
+++ src/main/java/christmas/util/MenuValidator.java
@@ -1,26 +1,57 @@
 package christmas.util;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class MenuValidator {
 
     private String menuPattern = "^[A-Za-z가-힣]+\\-+[\\d]+$";
 
+
     public void validate(String inputMenu) {
         try {
-            List<String> splitInputMenus = List.of(inputMenu.split(","));
-            validateFormat(splitInputMenus);
-            validateMenuDuplicate(splitInputMenus);
+            List<String> firstSplitByComma = List.of(inputMenu.split(","));
+            validateFormat(firstSplitByComma);
+            Map<String, Integer> menuInfo = putMenu(firstSplitByComma);
+            validateMenuCountWrongNumber(menuInfo);
+            validateMenuDuplicate(menuInfo);
         }catch (IllegalArgumentException error){
             throw new IllegalArgumentException(error.getMessage());
         }
 
     }
 
-    private void validateMenuDuplicate(List<String> splitInputMenus) {
+    private void validateMenuCountWrongNumber(Map<String, Integer> menuInfo) {
+        for (Entry<String, Integer> nowMenuInfo : menuInfo.entrySet()) {
+            if(nowMenuInfo.getValue() < 1 || nowMenuInfo.getValue() > 20){
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+
+        }
+    }
+
+    private Map<String, Integer> putMenu(List<String> firstSplitByComma) {
+        Map<String, Integer> inputMenuMap = new HashMap<>();
+        for (String splitInput : firstSplitByComma) {
+            List<String> secondSplitByHighPone = List.of(splitInput.split("-"));
+            int menuCount = Integer.parseInt(secondSplitByHighPone.get(1));
+            inputMenuMap.put(secondSplitByHighPone.get(0), menuCount);
+        }
+        return inputMenuMap;
+    }
+
+    private void validateMenuDuplicate(Map<String, Integer> menuInfo) {
+        Set<Entry<String, Integer>> menuInfoNoDuplicate = new HashSet<>(menuInfo.entrySet());
+
 
     }
+
 
     private void validateFormat(List<String> splitInputMenus) {
         for (String splitInputMenu : splitInputMenus) {
