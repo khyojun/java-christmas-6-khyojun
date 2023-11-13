7c2c1d656c127b6c132a189f1c23b7ed120216b6
김효준 <khyojun1104@gmail.com>
2023-11-14 01:06:15 +0900

    feat(christmas): 객체 추가 분리하여 평일, 주말 할인 구분!

--- src/main/java/christmas/domain/WeekSaleStatus.java
+++ src/main/java/christmas/domain/WeekSaleStatus.java
@@ -1,2 +1,21 @@
-package christmas.domain;public class WeekSaleStatus {
+package christmas.domain;
+
+public class WeekSaleStatus {
+
+
+    private long salePrice;
+    private String weekDay;
+
+    public WeekSaleStatus(long salePrice, String weekDay) {
+        this.salePrice = salePrice;
+        this.weekDay = weekDay;
+    }
+
+    public long getSalePrice() {
+        return salePrice;
+    }
+
+    public String getWeekDay() {
+        return weekDay;
+    }
 }
