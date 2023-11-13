be1f87770d1b2f0cbd96f4865673e745b612591c
김효준 <khyojun1104@gmail.com>
2023-11-13 17:59:00 +0900

    feat(christmas): 날짜 입력 이전 출력문 작성!

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,5 +1,6 @@
 package christmas.controller;
 
+import christmas.util.Validator;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
@@ -8,13 +9,21 @@ public class ChristmasController {
 
     private InputView inputView;
     private OutputView outputView;
+
+    private Validator validator;
+
     public ChristmasController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.validator = new Validator();
+
     }
 
 
     public void startOrder() {
+
+        outputView.beforeInputDate();
+        validator.dateValidate(inputView.readDate());
 
 
     }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -12,4 +12,9 @@ public class OutputView {
             instance = new OutputView();
         return instance;
     }
+
+    public void beforeInputDate() {
+        System.out.printf(
+            "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n");
+    }
 }
