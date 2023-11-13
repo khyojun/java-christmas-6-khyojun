8e7ddc0578e619748b54c67637012d70aa25356a
김효준 <khyojun1104@gmail.com>
2023-11-13 17:27:40 +0900

    chore(christmas): controller main 연결 시키는 작업 진행!

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,5 +1,21 @@
 package christmas.controller;
 
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class ChristmasController {
 
+
+    private InputView inputView;
+    private OutputView outputView;
+    public ChristmasController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+
+    public void startOrder() {
+
+
+    }
 }
