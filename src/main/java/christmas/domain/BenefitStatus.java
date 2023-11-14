8c293051e00072773416bcd2d50105a3a4dc6196
김효준 <khyojun1104@gmail.com>
2023-11-14 15:47:50 +0900

    fix(christmas): BenefitStatus 관련 메서드 이름 수정!

--- src/main/java/christmas/domain/BenefitStatus.java
+++ src/main/java/christmas/domain/BenefitStatus.java
@@ -4,24 +4,21 @@ public class BenefitStatus {
 
     private final long dDaySalePrice;
     private final WeekSaleStatus weekSalePrice;
-    private final long specialSalePrice;
+    private final long giftPrice;
     private final long starDatePrice;
-
 
 
     private boolean none;
 
-    public BenefitStatus(long dDaySalePrice, WeekSaleStatus weekSaleStatus, long specialSalePrice, long starDatePrice) {
+    public BenefitStatus(long dDaySalePrice, WeekSaleStatus weekSaleStatus, long giftPrice,
+        long starDatePrice) {
         this.dDaySalePrice = dDaySalePrice;
         this.weekSalePrice = weekSaleStatus;
-        this.specialSalePrice = specialSalePrice;
+        this.giftPrice = giftPrice;
         this.starDatePrice = starDatePrice;
         this.none = false;
     }
 
-    public long getSpecialDatePrice() {
-        return this.specialSalePrice;
-    }
 
     public long getStarDatePrice() {
         return this.starDatePrice;
@@ -32,19 +29,19 @@ public class BenefitStatus {
     }
 
     public long getGiftSalePrice() {
-        return specialSalePrice;
+        return giftPrice;
     }
 
     public WeekSaleStatus getWeekSaleStatus() {
         return this.weekSalePrice;
     }
 
-    public boolean isNone(){
+    public boolean isNone() {
         return none;
     }
 
     public void hasNothing() {
-        this.none=true;
+        this.none = true;
     }
 
 }
