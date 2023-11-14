fd8e671ea6bf438c89999b342956f8858022e5dd
김효준 <khyojun1104@gmail.com>
2023-11-15 01:11:08 +0900

    test(christmas): 평일 케이스 할인 테스트!

--- src/test/java/christmas/service/WeekServiceTest.java
+++ src/test/java/christmas/service/WeekServiceTest.java
@@ -1,5 +1,48 @@
 package christmas.service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 public class WeekServiceTest {
+
+    WeekService weekService;
+
+    @BeforeEach
+    void init() {
+        weekService = new WeekService();
+    }
+
+
+    @ParameterizedTest
+    @DisplayName("평일일 때의 경우")
+    @MethodSource("weekDayCase")
+    void weekDayTest(Map<String, Integer> menuInfo, Integer date) {
+        Assertions.assertThat(weekService.calculateWeekBenefit(menuInfo,date).getSalePrice()).isEqualTo(-4046);
+    }
+
+
+    static Stream<Arguments> weekDayCase() {
+        return Stream.of(
+            Arguments.arguments(new HashMap() {{
+                put("아이스크림", 2);
+                put("바비큐립", 3);
+            }}, 3),
+            Arguments.arguments(new HashMap() {{
+                put("아이스크림", 2);
+                put("바비큐립", 3);
+            }}, 4),
+            Arguments.arguments(new HashMap() {{
+                put("아이스크림", 2);
+                put("바비큐립", 3);
+            }}, 7)
+        );
+    }
 
 }
