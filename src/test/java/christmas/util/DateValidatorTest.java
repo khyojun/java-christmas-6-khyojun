4982ce711e02c3083edf2ea3d151edf9a6690ab6
김효준 <khyojun1104@gmail.com>
2023-11-14 22:21:12 +0900

    test(christmas): 입력 값들에 대한 validator test 클래스들 생성!

---
+++ src/test/java/christmas/util/DateValidatorTest.java
@@ -0,0 +1,19 @@
+package christmas.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class DateValidatorTest {
+
+
+
+
+
+}
---
+++ src/test/java/christmas/util/MenuValidatorTest.java
@@ -0,0 +1,19 @@
+package christmas.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MenuValidatorTest {
+
+
+
+
+
+
+
+
+}
