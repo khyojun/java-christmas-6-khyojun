5b982e61dfb698e5a57257b7692c8d3b23412802
김효준 <khyojun1104@gmail.com>
2023-11-14 22:38:09 +0900

    refactor(christmas): 조건문 메서드 추출!

--- src/main/java/christmas/service/WeekService.java
+++ src/main/java/christmas/service/WeekService.java
@@ -3,7 +3,6 @@ package christmas.service;
 import static christmas.constant.DateConstant.MONTH;
 import static christmas.constant.DateConstant.YEAR;
 
-import christmas.constant.DateConstant;
 import christmas.domain.Menu;
 import christmas.domain.WeekSaleStatus;
 import java.time.LocalDate;
@@ -19,17 +18,24 @@ public class WeekService {
     private static final String WEEKEND_SALE = "주말 할인";
     private static final String WEEKDAY_SALE = "평일 할인";
 
+    private static final int FRIDAY_WEEK_NUM = 5;
+    private static final int SATURDAY_WEEK_NUM = 6;
+
 
     public WeekSaleStatus calculateWeekBenefit(Map<String, Integer> menuInfo, Integer date) {
         return calculate(menuInfo, date);
     }
 
     private WeekSaleStatus calculate(Map<String, Integer> menuInfo, int date) {
-        int value = LocalDate.of(YEAR.getDateNumber(), MONTH.getDateNumber(), date).getDayOfWeek().getValue();
-        if (value == 5 || value == 6) {
+        int weekDay = LocalDate.of(YEAR.getDateNumber(), MONTH.getDateNumber(), date).getDayOfWeek().getValue();
+        if (isWeekend(weekDay)) {
             return new WeekSaleStatus(checkBenefitMenu(menuInfo, MAIN_MENU), WEEKEND_SALE);
         }
         return new WeekSaleStatus(checkBenefitMenu(menuInfo, DESERT_MENU), WEEKDAY_SALE);
+    }
+
+    private boolean isWeekend(int weekDay) {
+        return weekDay == FRIDAY_WEEK_NUM || weekDay == SATURDAY_WEEK_NUM;
     }
 
     private long checkBenefitMenu(Map<String, Integer> menuInfo, String menuCategory) {
