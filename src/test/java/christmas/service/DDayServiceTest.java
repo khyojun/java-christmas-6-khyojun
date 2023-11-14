af93a12bd586f35a1f3fbac671f491892bd1891e
김효준 <khyojun1104@gmail.com>
2023-11-14 23:38:55 +0900

    test(christmas): 1-25일 날짜 이내 할인 테스트!

--- src/test/java/christmas/service/DDayServiceTest.java
+++ src/test/java/christmas/service/DDayServiceTest.java
@@ -2,11 +2,31 @@ package christmas.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class DDayServiceTest {
 
-    @Test
-    void calculateDdayBenefit() {
+    DDayService dDayService;
+
+    @BeforeEach
+    void beforeAll() {
+       dDayService = new DDayService();
     }
+
+    @ParameterizedTest
+    @DisplayName("1-25일 날짜 이내의 할인가격 검증")
+    @ValueSource(ints = {1,2,3,25})
+    void calculateDdayBenefit(int date) {
+        Assertions.assertThat(dDayService.calculateDdayBenefit(date)).isLessThan(0);
+    }
+
+
+
+
 }
