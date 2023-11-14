15966e97b93e440a857e59eb3a7266e0a4346ee6
김효준 <khyojun1104@gmail.com>
2023-11-14 23:56:28 +0900

    test(christmas): 증정 선물 이벤트에 대한 테스트 코드 작성!

---
+++ src/test/java/christmas/service/GiftServiceTest.java
@@ -0,0 +1,42 @@
+package christmas.service;
+
+import static christmas.domain.Menu.BEVERAGE_CHAMPAGNE;
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.domain.Menu;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class GiftServiceTest {
+
+
+    GiftService giftService;
+
+    @BeforeEach
+    void init(){
+        giftService = new GiftService();
+    }
+
+
+
+    @ParameterizedTest
+    @DisplayName("증정 선물 시 가격이 기준을 넘는 경우")
+    @ValueSource(ints = {125000, 140000})
+    void calculateGiftBenefit(int calculateTotalMoney) {
+        Assertions.assertThat(giftService.calculateGiftBenefit(calculateTotalMoney)).isEqualTo(
+            BEVERAGE_CHAMPAGNE.getPrice()*-1);
+    }
+
+    @ParameterizedTest
+    @DisplayName("증정 선물 시 가격이 기준을 넘지 않는 경우")
+    @ValueSource(ints = {100000, 5000})
+    void calculateGiftBenefitNotOverStandard(int calculateTotalMoney) {
+        Assertions.assertThat(giftService.calculateGiftBenefit(calculateTotalMoney)).isEqualTo(
+            0);
+    }
+}
