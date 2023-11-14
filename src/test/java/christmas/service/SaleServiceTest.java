8a4d7136fa9969e201db59887e93bd76ef03f086
김효준 <khyojun1104@gmail.com>
2023-11-14 21:54:03 +0900

    test(christmas): service 쪽 테스트 코드 작성을 위한 초기 셋팅!

---
+++ src/test/java/christmas/service/SaleServiceTest.java
@@ -0,0 +1,16 @@
+package christmas.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SaleServiceTest {
+
+    @Test
+    void saleCalculate() {
+    }
+
+    @Test
+    void calculateTotalSalePrice() {
+    }
+}
---
+++ src/test/java/christmas/service/StarDateServiceTest.java
@@ -0,0 +1,12 @@
+package christmas.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StarDateServiceTest {
+
+    @Test
+    void calculateStarDateSale() {
+    }
+}
