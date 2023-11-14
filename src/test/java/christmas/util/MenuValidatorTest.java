f4702656e53449c04aa6a2adc3d3ccaceb301bad
김효준 <khyojun1104@gmail.com>
2023-11-14 22:44:48 +0900

    test(christmas): 콤마 갯수 잘못된 경우 테스트!

--- src/test/java/christmas/util/MenuValidatorTest.java
+++ src/test/java/christmas/util/MenuValidatorTest.java
@@ -2,6 +2,8 @@ package christmas.util;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,7 +11,23 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class MenuValidatorTest {
 
+    MenuValidator menuValidator;
 
+
+    @BeforeEach
+    void init(){
+        menuValidator = new MenuValidator();
+    }
+
+
+
+    @ParameterizedTest
+    @DisplayName("comma 갯수가 잘못 입력된 경우!")
+    @ValueSource(strings = {"menu-1,,", ",,menu-1", "menu-1,,,menu-2"})
+    void validateNoComma(String inputMenu){
+        Assertions.assertThatThrownBy(() -> menuValidator.validate(inputMenu)).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR]");
+    }
 
 
 
